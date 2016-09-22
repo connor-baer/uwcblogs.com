e0fcb467e6b6a8c6e01c82c0c568564eb9a7162a
Connor Bär <hello@connorbaer.io>
2016-09-22 12:10:59 +0200

    Fix js code style errors.

--- _js/scripts.js
+++ _js/scripts.js
@@ -1,10 +1,13 @@
+/* jshint -W117 */
+/* jshint -W098 */
+
 // HEADROOM //
 
 // Grab an element.
 var hrBody = document.body;
 
 // Construct an instance of Headroom, passing the element.
-var headroom = new Headroom(hrBody, {
+var headroom = new Headroom( hrBody, {
 
   // Vertical offset in px before element is first unpinned.
   offset: 64,
@@ -12,23 +15,23 @@ var headroom = new Headroom(hrBody, {
   // Scroll tolerance in px before state changes for up/down scroll.
   tolerance: {
     up: 10,
-    down: 5,
-  },
-});
+    down: 5
+  }
+} );
 
 // Initialise.
 headroom.init();
 
 // SMOOTHSCROLL //
 
-smoothScroll.init({
+smoothScroll.init( {
 
   // Easing pattern to use.
   easing: 'easeInOutCubic',
 
   // How far to offset the scrolling anchor location in pixels.
-  offset: 70,
-});
+  offset: 70
+} );
 
 // LISTJS //
 
@@ -38,23 +41,23 @@ var searchOptions = {
     'country',
     'language',
     {
-      data: ['year'],
-    },
-  ],
+      data: [ 'year' ]
+    }
+  ]
 };
 
-var lists = document.getElementsByClassName('js-list');
-var colleges = document.getElementsByClassName('js-college');
-var years = document.getElementsByClassName('js-years');
+var lists = document.getElementsByClassName( 'js-list' );
+var colleges = document.getElementsByClassName( 'js-college' );
+var years = document.getElementsByClassName( 'js-years' );
 
 var data = [];
 
-for (var i = 0; i < lists.length; ++i) {
-  var listId = lists[i].id;
-  data[i] = new List(listId, searchOptions);
+for ( var i = 0; i < lists.length; ++i ) {
+  var listId = lists[ i ].id;
+  data[ i ] = new List( listId, searchOptions );
 }
 
-var searchInput = document.getElementById('js-search');
+var searchInput = document.getElementById( 'js-search' );
 
 function searchAll() {
 
@@ -62,36 +65,36 @@ function searchAll() {
   var searchValue = searchInput.value;
 
   // Search all lists.
-  for (var i = 0; i < lists.length; ++i) {
-    data[i].search(searchValue);
+  for ( var i = 0; i < lists.length; ++i ) {
+    data[ i ].search( searchValue );
   }
 
   // Hide all year numbers without blogs.
-  for (var j = 0; j < years.length; ++j) {
-    if (years[j].hasChildNodes()) {
-      years[j].parentNode.style.display = 'block';
+  for ( var j = 0; j < years.length; ++j ) {
+    if ( years[ j ].hasChildNodes() ) {
+      years[ j ].parentNode.style.display = 'block';
     } else {
-      years[j].parentNode.style.display = 'none';
+      years[ j ].parentNode.style.display = 'none';
     }
   }
 
   // Hide all college names with all years hidden.
-  for (var k = 0; k < colleges.length; ++k) {
-    var yearsInCollege = colleges[k].getElementsByClassName('js-list');
+  for ( var k = 0; k < colleges.length; ++k ) {
+    var yearsInCollege = colleges[ k ].getElementsByClassName( 'js-list' );
     var empty = 0;
 
-    for (var m = 0; m < yearsInCollege.length; ++m) {
-      if (yearsInCollege[m].style.display === 'none') {
+    for ( var m = 0; m < yearsInCollege.length; ++m ) {
+      if ( yearsInCollege[ m ].style.display === 'none' ) {
         empty++;
       }
     }
 
-    if (empty === yearsInCollege.length) {
-      colleges[k].style.display = 'none';
+    if ( empty === yearsInCollege.length ) {
+      colleges[ k ].style.display = 'none';
     } else {
-      colleges[k].style.display = 'block';
+      colleges[ k ].style.display = 'block';
     }
   }
 }
 
-searchInput.addEventListener('keyup', searchAll);
+searchInput.addEventListener( 'keyup', searchAll );
