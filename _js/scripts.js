d03b9d54d5e48a17bc256dad2ab40aaf4210eed3
Connor Bär <hello@connorbaer.io>
2016-09-23 15:45:32 +0200

    Adds Intercom.

--- _js/scripts.js
+++ _js/scripts.js
@@ -2,100 +2,103 @@
 /* jshint -W098 */
 /* jshint -W070 */
 
-// HEADROOM //
+document.addEventListener('turbolinks:load', function () {
 
-// Grab an element.
-var hrBody = document.body;
+  // HEADROOM //
 
-// Construct an instance of Headroom, passing the element.
-var headroom = new Headroom(hrBody, {
+  // Grab an element.
+  var hrBody = document.body;
 
-  // Vertical offset in px before element is first unpinned.
-  offset: 64,
+  // Construct an instance of Headroom, passing the element.
+  var headroom = new Headroom(hrBody, {
 
-  // Scroll tolerance in px before state changes for up/down scroll.
-  tolerance: {
-    up: 10,
-    down: 5,
-  },
-});
+    // Vertical offset in px before element is first unpinned.
+    offset: 64,
 
-// Initialise.
-headroom.init();
-
-// SMOOTHSCROLL //
-
-smoothScroll.init({
-
-  // Easing pattern to use.
-  easing: 'easeInOutCubic',
-
-  // How far to offset the scrolling anchor location in pixels.
-  offset: 70,
-});
-
-// LISTJS //
-
-var searchOptions = {
-  valueNames: [
-    'name',
-    'country',
-    'language',
-    {
-      data: ['year'],
+    // Scroll tolerance in px before state changes for up/down scroll.
+    tolerance: {
+      up: 10,
+      down: 5,
     },
-  ],
-};
+  });
 
-var lists = document.getElementsByClassName('js-list');
-var colleges = document.getElementsByClassName('js-college');
-var years = document.getElementsByClassName('js-years');
+  // Initialise.
+  headroom.init();
 
-var data = [];
+  // SMOOTHSCROLL //
 
-for (var i = 0; i < lists.length; ++i) {
-  var listId = lists[i].id;
-  data[i] = new List(listId, searchOptions);
-}
+  smoothScroll.init({
 
-var searchInput = document.getElementById('js-search');
+    // Easing pattern to use.
+    easing: 'easeInOutCubic',
 
-function searchAll() {
+    // How far to offset the scrolling anchor location in pixels.
+    offset: 70,
+  });
 
-  // Get search value.
-  var searchValue = searchInput.value;
+  // LISTJS //
 
-  // Search all lists.
+  var searchOptions = {
+    valueNames: [
+      'name',
+      'country',
+      'language',
+      {
+        data: ['year'],
+      },
+    ],
+  };
+
+  var lists = document.getElementsByClassName('js-list');
+  var colleges = document.getElementsByClassName('js-college');
+  var years = document.getElementsByClassName('js-years');
+
+  var data = [];
+
   for (var i = 0; i < lists.length; ++i) {
-    data[i].search(searchValue);
+    var listId = lists[i].id;
+    data[i] = new List(listId, searchOptions);
   }
 
-  // Hide all year numbers without blogs.
-  for (var j = 0; j < years.length; ++j) {
-    if (years[j].hasChildNodes()) {
-      years[j].parentNode.style.display = 'block';
-    } else {
-      years[j].parentNode.style.display = 'none';
+  var searchInput = document.getElementById('js-search');
+
+  function searchAll() {
+
+    // Get search value.
+    var searchValue = searchInput.value;
+
+    // Search all lists.
+    for (var i = 0; i < lists.length; ++i) {
+      data[i].search(searchValue);
     }
-  }
 
-  // Hide all college names with all years hidden.
-  for (var k = 0; k < colleges.length; ++k) {
-    var yearsInCollege = colleges[k].getElementsByClassName('js-list');
-    var empty = 0;
-
-    for (var m = 0; m < yearsInCollege.length; ++m) {
-      if (yearsInCollege[m].style.display === 'none') {
-        empty++;
+    // Hide all year numbers without blogs.
+    for (var j = 0; j < years.length; ++j) {
+      if (years[j].hasChildNodes()) {
+        years[j].parentNode.style.display = 'block';
+      } else {
+        years[j].parentNode.style.display = 'none';
       }
     }
 
-    if (empty === yearsInCollege.length) {
-      colleges[k].style.display = 'none';
-    } else {
-      colleges[k].style.display = 'block';
+    // Hide all college names with all years hidden.
+    for (var k = 0; k < colleges.length; ++k) {
+      var yearsInCollege = colleges[k].getElementsByClassName('js-list');
+      var empty = 0;
+
+      for (var m = 0; m < yearsInCollege.length; ++m) {
+        if (yearsInCollege[m].style.display === 'none') {
+          empty++;
+        }
+      }
+
+      if (empty === yearsInCollege.length) {
+        colleges[k].style.display = 'none';
+      } else {
+        colleges[k].style.display = 'block';
+      }
     }
   }
-}
 
-searchInput.addEventListener('keyup', searchAll);
+  searchInput.addEventListener('keyup', searchAll);
+});
