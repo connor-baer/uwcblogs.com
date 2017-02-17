54a24fb1fa08860d1542dc0f34d7956b85178070
Connor Bär <hello@connorbaer.io>
2017-02-17 21:54:17 +0100

    Minor fixes.

--- public/github-webhook.php
+++ public/github-webhook.php
@@ -6,9 +6,9 @@ $repo          = '/srv/users/serverpilot/apps/uwcblogs';
 $branch        = 'master';
 $output        = array();
 
-// update github Repo
+// Update the repo.
 $output[] = date( 'Y-m-d, H:i:s', time() ) . "\n";
 $output[] = "GitHub Pull\n============================\n" . shell_exec( 'cd ' . $repo . ' && git pull origin ' . $branch );
 
-// redirect output to logs
-file_put_contents( rtrim( getcwd(), '/' ) . '/_github-log.txt', implode( "\n", $output ) . "\n----------------------------\n", FILE_APPEND );
+// Redirect output to logs.
+file_put_contents( rtrim( getcwd(), '/' ) . '/github-log.txt', implode( "\n", $output ) . "\n----------------------------\n", FILE_APPEND );
