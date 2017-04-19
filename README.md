aa5eb1abcf90e97a17098f4c52ab104b7dcb1a99
Connor Bär <hello@connorbaer.co>
2017-04-19 20:36:51 +0200

    Code cleanup. ✨

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/2d8734becc4c452ab3c4db9875435a55)](https://www.codacy.com/app/connor_baer/uwcblogs?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=uwc/uwcblogs&amp;utm_campaign=Badge_Grade)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/2d8734becc4c452ab3c4db9875435a55)](https://www.codacy.com/app/connor_baer/uwcblogs?utm_source=github.com&utm_medium=referral&utm_content=uwc/uwcblogs&utm_campaign=Badge_Grade)
 
 # UWC Blogs
 
 _A collection of over 350 blogs written by UWC students in 28 languages from 76 countries at the 17 United World Colleges._
 
-I strive to make this list as extensive as possible so that everybody can find a blog for their favorite college in their native language. This wouldn't be possible without you. Thanks to everybody who submitted a blog! You have a blog yourself or know one that isn't listed here? Submit the link [here](http://uwcblogs.com/submit) and I'll add it as soon as possible. Thank you!
+When I started UWC Blogs in 2013, my intention was to give UWC applicants a glimpse into life at UWC. At the time I was waiting impatiently to start my own UWC experience at [UWC Waterford Kamhlaba](https://uwcblogs.com/uwc-waterford-kamhlaba). I had soaked up every bit of UWC promotional material, but I still couldn't quite imagine what UWC would be like. So I read through every blog about UWC I could find and eventually created [UWC Blogs](https://uwcblogs.com) to share the blogs with other UWC hopefuls.
+
+I strive to make the list as extensive as possible so that everybody can find a blog for their favorite college in their native language. This wouldn't be possible without you. Thanks to everybody who submitted a blog! Do you blog about UWC or know a blog that isn't listed here? [Submit the link](https://uwcblogs.com/submit) and I'll add it as soon as possible.
--- source/js/vue.js
+++ source/js/vue.js
@@ -1,24 +1,35 @@
 /**
  * Vue Instance
+ *
+ * Vue loads, renders and filters the blogs.
  */
 new Vue( {
   el: '#app',
   delimiters: [ '[[', ']]' ],
   data: {
+    // Get search value from URL hash
     search: ( window.location.hash ? window.location.hash.substring( 1 ) : '' ),
     blogs: [],
   },
   mounted() {
     var self = this;
 
+    /**
+     * Fetch the blogs
+     *
+     * @param  {url} url  URL to fetch
+     * @param  {function} callback  Callback function
+     */
     function getData( url, callback ) {
 
       Vue.http.get( url ).then( response => {
+        // Push the paginated json packets onto the data object
         self.$data.blogs = response.body.data.reduce( function ( coll, item ) {
           coll.push( item );
           return coll;
         }, self.$data.blogs );
 
+        // Get the page URL to fetch
         let nextUrl = response.body.meta.pagination.links.next;
 
         if ( nextUrl ) {
@@ -33,6 +44,7 @@ new Vue( {
       } );
     }
 
+    // Set API URL according to the page path
     const path = ( window.location.pathname !== '/' ? window.location.pathname : '' );
     let url = '/api/blogs' + path + '.json';
 
@@ -41,13 +53,20 @@ new Vue( {
     } );
   },
   computed: {
+    /**
+     * Filter and sort the data
+     *
+     * @return  {object} filteredBlogs  The filtered and sorted data
+     */
     filteredBlogs() {
       var self = this;
 
+      // Use search input to filter the data across all properties
       let filterData = this.blogs.filter( function ( blog ) {
         return blog.firstName.toLowerCase().indexOf( self.search.toLowerCase() ) > -1 || blog.college.toLowerCase().indexOf( self.search.toLowerCase() ) > -1 || blog.year.toLowerCase().indexOf( self.search.toLowerCase() ) > -1 || blog.countries.toLowerCase().indexOf( self.search.toLowerCase() ) > -1 || blog.languages.toLowerCase().indexOf( self.search.toLowerCase() ) > -1;
       } );
 
+      // Sort the colleges alphabetically
       function sortColleges( a, b ) {
         const criterionA = a.college.toUpperCase();
         const criterionB = b.college.toUpperCase();
@@ -62,6 +81,7 @@ new Vue( {
         return comparison;
       }
 
+      // Group the blogs by college
       return _.chain( filterData )
         .groupBy( 'college' )
         .toPairs()
@@ -72,17 +92,29 @@ new Vue( {
     }
   },
   watch: {
+    /**
+     * Save search string to hash
+     *
+     * @param  {string} value  The search input value
+     */
     search: function ( value ) {
       var hash = encodeURIComponent( value );
 
       if ( hash !== '' ) {
+        // Set the hash to the search input value
         window.location.hash = hash;
       } else {
+        // Remove trailing # to prevent scrolling
         history.replaceState( '', document.title, window.location.pathname + window.location.search );
       }
     }
   },
   methods: {
+    /**
+     * Slugify string
+     *
+     * @param  {string} string  String to be slugified
+     */
     slugify: function ( string ) {
       return string
         .toString()
