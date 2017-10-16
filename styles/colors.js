634f924abdeb5f9a3ff09978559ef7593f52e2a3
Connor Bär <connor.baer@sumup.com>
2017-10-16 23:06:15 +0200

    Remove old styles.

--- styles/colors.js
+++
@@ -1,9 +0,0 @@
-import oc from 'open-color/open-color.json';
-
-const colors = {
-  ...oc,
-  primary: '#1fb7e3',
-  secondary: '#00ccd2'
-};
-
-export { colors };
--- styles/index.js
+++
@@ -1,6 +0,0 @@
-import { animations } from './animations';
-import { breakpoints } from './breakpoints';
-import { colors } from './colors';
-import { fonts } from './fonts';
-
-export { animations, breakpoints, colors, fonts };
