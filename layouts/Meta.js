4cfe9aef9d19ac5839275280d9cd556aa7d3b2da
Connor Bär <connor.baer@sumup.com>
2017-10-17 22:38:13 +0200

    Bug fixes.

--- layouts/Meta.js
+++ layouts/Meta.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { flow } from 'lodash';
 import NProgress from 'nprogress';
 import { Router } from '../server/routes/next';
 import withSite from 'change/withSite';
