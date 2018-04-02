30caa2e0c7b5fb1d9a1c2b7716fa97c4fa62d394
Connor Bär <connor.baer@sumup.com>
2018-04-02 21:16:43 +0200

    Move progress indicator to _document

--- src/layouts/Meta.js
+++ src/layouts/Meta.js
@@ -1,25 +1,14 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { flow } from 'lodash';
-import NProgress from 'nprogress';
-import { Router } from '../server/routes/next';
-import { withSite, withStyles } from 'change';
-import Styles from './Styles';
-
-Router.onRouteChangeStart = () => NProgress.start();
-Router.onRouteChangeComplete = () => NProgress.done();
-Router.onRouteChangeError = () => NProgress.done();
+import SiteContext from '../context/SiteContext';
 
 class Meta extends Component {
   static propTypes = {
     title: PropTypes.string,
     index: PropTypes.bool,
     follow: PropTypes.bool,
-    head: PropTypes.element,
-    site: PropTypes.shape({
-      name: PropTypes.string
-    })
+    head: PropTypes.element
   };
 
   componentDidMount() {
@@ -53,67 +42,40 @@ class Meta extends Component {
   }
 
   render() {
-    const { title, index, follow, head, site, styles } = this.props;
-    const { name } = site;
-    const { colors } = styles;
+    const { title, index, follow, head } = this.props;
     const pageTitle = title ? `${title} — ` : '';
     return (
-      <div>
-        <Head>
-          <title>{`${pageTitle}${name}`}</title>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="robots"
-            content={`${index ? 'index' : 'noindex'}, ${
-              follow ? 'follow' : 'nofollow'
-            }`}
-          />
-          <meta
-            name="google-site-verification"
-            content="UXWTlQN6FmtFKirHUeXDKh13WSjo_v5g21dRjbhqMNw"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/static/favicons/favicon-96x96.png"
-          />
-          {head}
-        </Head>
-        <Styles />
-        <style jsx global>{`
-          #nprogress {
-            pointer-events: none;
-
-            & .bar {
-              background: ${colors.primary};
-              position: fixed;
-              z-index: 1031;
-              top: 0;
-              left: 0;
-              width: 100%;
-              height: 2px;
-            }
-
-            & .peg {
-              display: block;
-              position: absolute;
-              right: 0px;
-              width: 100px;
-              height: 100%;
-              box-shadow: 0 0 10px ${colors.primary}, 0 0 5px ${colors.primary};
-              opacity: 1;
-              transform: rotate(3deg) translate(0px, -4px);
-            }
-          }
-        `}</style>
-      </div>
+      <SiteContext.Consumer>
+        {site => (
+          <Head>
+            <title>{`${pageTitle}${site.name}`}</title>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+              name="robots"
+              content={`${index ? 'index' : 'noindex'}, ${follow
+                ? 'follow'
+                : 'nofollow'}`}
+            />
+            <meta
+              name="google-site-verification"
+              content="UXWTlQN6FmtFKirHUeXDKh13WSjo_v5g21dRjbhqMNw"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              href="/static/favicons/favicon-96x96.png"
+            />
+            {head}
+          </Head>
+        )}
+      </SiteContext.Consumer>
     );
   }
 }
 
-export default flow(withSite, withStyles)(Meta);
+export default Meta;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,44 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { injectGlobal } from 'emotion';
 import { extractCritical } from 'emotion-server';
+import NProgress from 'nprogress';
+import { Router } from '../../server/routes/next';
 
 import { globalStyles } from '../shared/styles';
 import { change } from '../shared/themes';
 
-injectGlobal(globalStyles({ theme: change }));
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
+
+const customStyles = `
+  #nprogress {
+    pointer-events: none;
+
+    & .bar {
+      background: ${change.colors.p500};
+      position: fixed;
+      z-index: 1031;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 2px;
+    }
+
+    & .peg {
+      display: block;
+      position: absolute;
+      right: 0px;
+      width: 100px;
+      height: 100%;
+      box-shadow: 0 0 10px ${change.colors.p500}, 0 0 5px ${change.colors.p500};
+      opacity: 1;
+      transform: rotate(3deg) translate(0px, -4px);
+    }
+  }
+`;
+
+injectGlobal(globalStyles({ theme: change, custom: customStyles }));
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
