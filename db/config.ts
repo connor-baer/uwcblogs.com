6d25bbf2c9feb4a9cc9f2c2b550a8cfadc582926
Connor Bär <github@connorbaer.com>
2024-03-25 09:12:54 +0100

    Add DB schema and seed

---
+++ db/config.ts
@@ -0,0 +1,61 @@
+import { defineDb, defineTable, column } from 'astro:db';
+
+const College = defineTable({
+  columns: {
+    slug: column.text({ primaryKey: true }),
+    name: column.text(),
+    description: column.text(),
+    websiteUrl: column.text(),
+  },
+});
+
+const Country = defineTable({
+  columns: {
+    code: column.text({ primaryKey: true }),
+    name: column.text({ unique: true }),
+  },
+});
+
+const Language = defineTable({
+  columns: {
+    code: column.text({ primaryKey: true }),
+    name: column.text({ unique: true }),
+    nativeName: column.text(),
+  },
+});
+
+const Blog = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    firstName: column.text(),
+    url: column.text(),
+    year: column.number(),
+    collegeId: column.text({ references: () => College.columns.slug }),
+  },
+});
+
+const BlogsToCountries = defineTable({
+  columns: {
+    blogId: column.number({ references: () => Blog.columns.id }),
+    countryId: column.text({ references: () => Country.columns.code }),
+  },
+});
+
+const BlogsToLanguages = defineTable({
+  columns: {
+    blogId: column.number({ references: () => Blog.columns.id }),
+    languageId: column.text({ references: () => Language.columns.code }),
+  },
+});
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: {
+    Blog,
+    College,
+    Country,
+    Language,
+    BlogsToCountries,
+    BlogsToLanguages,
+  },
+});
