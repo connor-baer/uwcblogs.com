b1794eb4b3abd3afee2f9d47ff7b120f38948d29
Connor Bär <github@connorbaer.com>
2024-03-25 09:11:35 +0100

    Install Astro DB

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://uwcblogs.com',
+  integrations: [db()],
 });
