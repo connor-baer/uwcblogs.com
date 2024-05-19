e82ed717a34f8f08e7596198249f1b5bf6179e18
Connor Bär <github@connorbaer.com>
2024-05-19 13:40:42 +0200

    Enable experimental actions

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel({
+    imageService: true,
+    functionPerRoute: false,
+  }),
   site: 'https://uwcblogs.com',
   integrations: [db()],
+  experimental: {
+    actions: true,
+    security: {
+      csrfProtection: {
+        origin: true,
+      },
+    },
+  },
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="../.astro/actions.d.ts" />
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
