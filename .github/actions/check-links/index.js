d2a59cd80e388c7f273bf0a21571a58ee568af69
Connor Bär <github@connorbaer.com>
2024-05-21 22:29:11 +0200

    Improve check-links logs

--- .github/actions/check-links/index.js
+++ .github/actions/check-links/index.js
@@ -3,56 +3,62 @@ import PQueue from 'p-queue';
 import ky from 'ky';
 
 async function run() {
-  try {
-    const queue = new PQueue({ concurrency: 5 });
-    const blogs = await ky.get('https://uwcblogs.com/blogs.json').json();
+  const queue = new PQueue({ concurrency: 5 });
+  const blogs = await ky.get('https://uwcblogs.com/blogs.json').json();
 
-    core.startGroup('Verifying links');
+  core.startGroup('Verifying links');
 
-    const responses = await Promise.all(
-      blogs.map(async (blog) =>
-        queue.add(async () => {
-          if (!URL.canParse(blog.url)) {
-            return { ...blog, status: 400 };
-          }
-          const response = await ky
-            .get(blog.url, { throwHttpErrors: false })
-            .catch((error) => {
-              let status = 500;
-              if (
-                error instanceof TypeError &&
-                error.cause &&
-                typeof value === 'object' &&
-                error.cause.code === 'ENOTFOUND'
-              ) {
-                status = 404;
-              }
-              core.error(blog.url, error);
-              return { status };
-            });
-          return { ...blog, status: response.status };
-        }),
-      ),
-    );
+  const responses = await Promise.all(
+    blogs.map(async (blog) =>
+      queue.add(async () => {
+        if (!URL.canParse(blog.url)) {
+          core.error(`Failed to parse ${blog.url}`);
+          return { ...blog, status: 400 };
+        }
+        core.debug(`Fetching ${blog.url}`);
+        const response = await ky
+          .get(blog.url, { throwHttpErrors: false })
+          .catch((error) => {
+            let status = 500;
+            if (
+              error instanceof TypeError &&
+              error.cause &&
+              typeof value === 'object' &&
+              error.cause.code === 'ENOTFOUND'
+            ) {
+              status = 404;
+            }
+            core.error(`Failed to fetch ${blog.url}`);
+            console.error(error);
+            return { status };
+          });
+        return { ...blog, status: response.status };
+      }),
+    ),
+  );
 
-    core.endGroup();
+  await queue.onIdle();
 
-    const successes = responses.filter(
-      (response) => response && response.status >= 200 && response.status < 300,
-    );
-    const failures = responses.filter(
-      (response) => !response || response.status >= 300,
-    );
+  core.endGroup();
 
-    core.info(`${successes.length} urls responded successfully`, successes);
-    core.warning(`${failures.length} urls responded with an error`, failures);
+  const successes = responses.filter(
+    (response) => response && response.status >= 200 && response.status < 300,
+  );
+  const failures = responses.filter(
+    (response) => !response || response.status >= 300,
+  );
 
-    if (failures.length > 0) {
-      core.setFailed(`Some urls returned errors`);
-    }
-  } catch (error) {
-    core.setFailed(error.message);
+  core.info(`${successes.length} urls responded successfully`);
+
+  if (failures.length > 0) {
+    core.setFailed(`${failures.length} urls returned errors`);
   }
 }
 
-run();
+run().catch((error) => {
+  if ('message' in error) {
+    core.setFailed(error.message);
+  } else {
+    core.setFailed('Unknown error: ' + JSON.stringify(error));
+  }
+});
