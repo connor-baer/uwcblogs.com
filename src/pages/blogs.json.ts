8ea04ace87fa1af82cf429be88f1572b2947a8f4
Connor Bär <github@connorbaer.com>
2024-05-21 22:47:31 +0200

    Exclude draft blogs

--- src/pages/blogs.json.ts
+++ src/pages/blogs.json.ts
@@ -1,7 +1,10 @@
 import type { APIRoute } from 'astro';
-import { Blog, db } from 'astro:db';
+import { db, eq, Blog } from 'astro:db';
 
 export const GET: APIRoute = async () => {
-  const blogs = await db.select({ id: Blog.id, url: Blog.url }).from(Blog);
+  const blogs = await db
+    .select({ id: Blog.id, url: Blog.url })
+    .from(Blog)
+    .where(eq(Blog.draft, false));
   return new Response(JSON.stringify(blogs));
 };
