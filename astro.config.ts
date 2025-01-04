a65f1ac109963dfae9943801b029a9e272a72db0
Connor Bär <github@connorbaer.com>
2025-01-04 15:47:22 +0100

    Remove obsolete output config

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
   adapter: vercel(),
   site: 'https://uwcblogs.com',
   integrations: [db()],
