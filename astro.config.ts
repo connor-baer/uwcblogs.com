20d8d29066492826423ce8b0e9d5852acb2b6bdf
Connor Bär <github@connorbaer.com>
2025-02-15 17:16:12 +0100

    Initialize database

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,11 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   site: 'https://uwcblogs.com',
   integrations: [db()],
 });
