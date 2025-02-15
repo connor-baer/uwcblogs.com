5038da313a2ca23bec155aec9812cfd8df8f841d
Connor Bär <github@connorbaer.com>
2025-02-15 21:36:59 +0100

    Enable Cloudflare's image service

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
+    imageService: 'cloudflare',
     platformProxy: {
       enabled: true,
     },
